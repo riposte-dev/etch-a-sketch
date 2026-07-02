799480ca24438161d742db07d9080d432127da6f
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-02 13:02:00 +0900

    Change 'tile' div color when mouse enters

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -10,5 +10,12 @@ for (let i = 0; i < 4; i++) {
         tile.className = "tile";
         tileRow.appendChild(tile);
     }
-
 }
+
+const tiles = document.querySelectorAll(".tile");
+
+tiles.forEach((tile) => {
+    tile.addEventListener('mouseenter', (e) => {
+        e.target.setAttribute("style", "background: cornflowerblue;");
+    })
+});
