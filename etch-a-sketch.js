b497ea89271430443c16f2cc28564b2ebd46331a
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-02 13:11:57 +0900

    Create new function createNewGrid()

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,24 +1,31 @@
 const container = document.querySelector(".container");
+let tiles = document.querySelectorAll(".tile");
 
-for (let i = 0; i < 16; i++) {
-    const tileRow = document.createElement("div");
-    tileRow.className = "tileRow";
-    container.appendChild(tileRow)
+function createNewGrid(length) {
+    /* Divide grid into rows as flex containers */
+    for (let i = 0; i < length; i++) {
+        const tileRow = document.createElement("div");
+        tileRow.className = "tileRow";
+        container.appendChild(tileRow)
 
-    for (let j = 0; j < 16; j++) {
-        const tile = document.createElement("div");
-        tile.className = "tile";
-        tileRow.appendChild(tile);
+        /* In each row, create flex items */
+        for (let j = 0; j < length; j++) {
+            const tile = document.createElement("div");
+            tile.className = "tile";
+            tileRow.appendChild(tile);
+        }
     }
+
+    tiles = document.querySelectorAll(".tile");
+
+    tiles.forEach((tile) => {
+        tile.addEventListener('mouseenter', (e) => {
+            e.target.setAttribute("style", "background: cornflowerblue;");
+        })
+    });
 }
 
-const tiles = document.querySelectorAll(".tile");
-
-tiles.forEach((tile) => {
-    tile.addEventListener('mouseenter', (e) => {
-        e.target.setAttribute("style", "background: cornflowerblue;");
-    })
-});
+createNewGrid(16); /* Create a default 16x16 grid */
 
 const button = document.querySelector("button");
 
