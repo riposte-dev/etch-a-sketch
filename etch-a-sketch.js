487ea29ac1c9465d2d1e6e961497756dc15ad835
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-07-02 11:58:21 +0900

    Parent 'tile' div's under a parent div with class 'tileRow'

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,14 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    const tile = document.createElement("div");
-    tile.className = "tile";
-    container.appendChild(tile);
+for (let i = 0; i < 4; i++) {
+    const tileRow = document.createElement("div");
+    tileRow.className = "tileRow";
+    container.appendChild(tileRow)
+
+    for (let j = 0; j < 4; j++) {
+        const tile = document.createElement("div");
+        tile.className = "tile";
+        tileRow.appendChild(tile);
+    }
+
 }
